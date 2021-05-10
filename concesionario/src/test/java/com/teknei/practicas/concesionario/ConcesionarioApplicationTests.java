3b699eb59f897c261003685e5e3dbb9f0dfc2e4a
Jorge Gutierrez <Jorge Gutierrez@LAPTOP-5V5S80CS>
2021-05-10 13:41:26 +0200

    Configuración de inicio

---
+++ concesionario/src/test/java/com/teknei/practicas/concesionario/ConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teknei.practicas.concesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
