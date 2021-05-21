810009b5d482b72943384c99bd5dbe068b87f23b
Jorge Gutierrez <Jorge Gutierrez@LAPTOP-5V5S80CS>
2021-05-21 10:14:11 +0200

    alta spring

--- concesionario/src/main/java/com/teknei/practicas/concesionario/controlador/IndexController.java
+++ concesionario/src/main/java/com/teknei/practicas/concesionario/controlador/IndexController.java
@@ -3,6 +3,7 @@ package com.teknei.practicas.concesionario.controlador;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
@@ -14,14 +15,19 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.teknei.practicas.concesionario.repositorios.AccesoDatosException;
 import com.teknei.practicas.concesionario.entidades.Coches;
+import com.teknei.practicas.concesionario.entidades.Marcas;
 import com.teknei.practicas.concesionario.repositorios.CocheRepositorio;
-import com.teknei.practicas.concesionario.repositorios.Dao;
+import com.teknei.practicas.concesionario.repositorios.MarcaRepositorio;
 
 import lombok.extern.java.Log;
 
@@ -32,13 +38,21 @@ public class IndexController {
 	
 	@Autowired
 	CocheRepositorio car;
+	MarcaRepositorio mar;
 	
 	private static final String SQL_INSERT = "INSERT INTO Coches (marca_id, modelo, matricula) VALUES (?, ?, ?)";
+	private static final String SQL_LISTA = "SELECT * FROM Coches c LEFT JOIN Marcas m ON c.marca_id = m.id WHERE id = ?";
 
 	@Autowired
 	private DataSource dataSource;
-
+	
 	@PostMapping("/alta")
+	public String alta(Coches nuevoCoche) {
+	    car.save(nuevoCoche);
+	    return "index";
+	}
+	
+	/**@PostMapping("/alta")
 	public String alta(Coches coches) {
 		try (Connection con = dataSource.getConnection();
 				PreparedStatement ps = con.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
@@ -50,7 +64,7 @@ public class IndexController {
 			int num = ps.executeUpdate();
 			
 			if(num != 1) {
-				throw new AccesoDatosException("Ha habido una incidencia en la inserción de cliente: " + num);
+				throw new AccesoDatosException("Ha habido una incidencia en la inserción del coche: " + num);
 			}
 			
 			ResultSet rs = ps.getGeneratedKeys();
@@ -63,29 +77,14 @@ public class IndexController {
 			log.info(coches.toString());
 			return "index";
 		} catch (Exception e) {
-			throw new AccesoDatosException("Error al insertar el cliente " + coches, e);
+			throw new AccesoDatosException("Error al insertar el coche " + coches, e);
 		}
-
-	}
-	
-	
-	//@RequestMapping(value = "listado", method = RequestMethod.POST)
-	//public String listado(Model modelo) {
-	//	modelo.addAttribute("coches", daoCoches.obtenerTodos());
-	//	System.out.println(daoCoches.obtenerTodos());
-	//	return "listado";
-	//}	
+	} */	
 
 	@GetMapping("/index")
 	public String get() { //doGet
 		return "index"; // request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request,response)
-	}
-	
-	//@RequestMapping("/listado")
-	//public String listado(Model modelo) {
-		//modelo.addAttribute("coches", daoCoches.obtenerTodos());
-	//	return "listado";
-	//}
+	}	
 	
 	@RequestMapping(value = "listado", method = RequestMethod.GET)
 	public String listado(ModelMap mp)
@@ -94,5 +93,11 @@ public class IndexController {
 		log.info(car.toString());
 		return "listado";
 	  }
+	
+	//@GetMapping("/listado/{id}")
+	//Marcas listado(@PathVariable Long id) { 
+	//    return mar.findById(id)
+	//      .orElseThrow(() -> new AccesoDatosException(id));
+	//}
 
 }
